54859297a5e02549c75d696e67fee95cb384da62
denz <denz@twiket.com>
2013-02-25 17:47:51 +0400

    fixe

--- bootstrap/chef-server/bootstrap-chef-server/attributes/default.rb
+++ bootstrap/chef-server/bootstrap-chef-server/attributes/default.rb
@@ -2,4 +2,4 @@ default['bootstrap']['type'] = :erchef
 default['chef_server']['user']['username'] = "chef_server"
 default['chef-server']['configuration']['rabbitmq']['vhost'] = "/chef"
 default['chef-server']['configuration']['rabbitmq']['user'] = "chef"
-default['chef-server']['configuration']['rabbitmq']['user'] = "chefrocks"
+default['chef-server']['configuration']['rabbitmq']['password'] = "chefrocks"
