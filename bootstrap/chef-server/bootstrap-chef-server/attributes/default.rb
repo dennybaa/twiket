846ff811eee9abbb0b09d7f278db42cebabad8e4
denz <denz@twiket.com>
2013-02-25 17:34:14 +0400

    added default attributes for rabbitmq

--- bootstrap/chef-server/bootstrap-chef-server/attributes/default.rb
+++ bootstrap/chef-server/bootstrap-chef-server/attributes/default.rb
@@ -1,2 +1,5 @@
 default['bootstrap']['type'] = :erchef
 default['chef_server']['user']['username'] = "chef_server"
+default['chef-server']['configuration']['rabbitmq']['vhost'] = "/chef"
+default['chef-server']['configuration']['rabbitmq']['user'] = "chef"
+default['chef-server']['configuration']['rabbitmq']['user'] = "chefrocks"
